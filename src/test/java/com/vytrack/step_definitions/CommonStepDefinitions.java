59310cfdf82e5454c246027d84813c41141d055d
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-10-21 22:06:24 -0400

    added negative_login sceanrio

--- src/test/java/com/vytrack/step_definitions/CommonStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/CommonStepDefinitions.java
@@ -1,12 +1,14 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.pages.LoginPage;
 import io.cucumber.java.en.Given;
 
 public class CommonStepDefinitions {
+    LoginPage loginPage = new LoginPage();
 
     @Given("user navigates to {string} and {string}")
     public void user_navigates_to_and(String string, String string2) {
-
+        loginPage.navigateTo(string, string2);
     }
 
 }
