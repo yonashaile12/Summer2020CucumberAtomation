5a3c417a1497010f1b97a3b57c07b3a9d85c6d22
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-10-21 21:26:59 -0400

    added negative_login sceanrio

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
         dryRun = false,
-        tags = "@negative_login or @parametrized_test",
+        tags = "@login",
         publish = true
 )
 public class CucumberRunner {
