10e495ee5def65e2766ee7e4d937f2443901ac38
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-10-21 21:44:08 -0400

    added negative_login sceanrio

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -9,8 +9,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
-        dryRun = false,
-        tags = "@login",
+        dryRun = true,
+        tags = "@add_car",
         publish = true
 )
 public class CucumberRunner {
---
+++ src/test/java/com/vytrack/step_definitions/CommonStepDefinitions.java
@@ -0,0 +1,12 @@
+package com.vytrack.step_definitions;
+
+import io.cucumber.java.en.Given;
+
+public class CommonStepDefinitions {
+
+    @Given("user navigates to {string} and {string}")
+    public void user_navigates_to_and(String string, String string2) {
+
+    }
+
+}
