a568056e8b43bd916559141cf7ad38620b3cf477
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-10-21 20:32:44 -0400

    added negative_login sceanrio

--- src/test/java/com/vytrack/step_definitions/Hooks.java
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -2,12 +2,34 @@ package com.vytrack.step_definitions;
 
 import com.vytrack.utils.Driver;
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
 
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Hooks name is not reserved. You may name this class in any way.
+ * For example: SuiteSetupAndTearDown
+ * Hooks triggered based on tags not class name or their location.
+ * These methods can be a part of any step definition class.
+ * Common practice is to store them in the separate class.
+ */
 public class Hooks {
 
+//    hook before = @BeforeMethod in TestNG
 //    hook after = @AfterMethod in TestNG
+//    it's not a good idea to mix implicit and explicit waits. It can lead to unexpectedly long waits.
+//    usually, we just use explicit and fluent waits.
+
+    @Before
+    public void setup() {
+        System.out.println("::: Starting Automation:::");
+        Driver.getDriver().manage().window().maximize();
+        Driver.getDriver().manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+
     @After
-    public void tearDown(){
+    public void tearDown() {
+        //close browser, close DB connection, close tunnel,capture screenshot of the error, etc..
         //this is a hook after
         //runs automatically after every test
         Driver.closeDriver();
