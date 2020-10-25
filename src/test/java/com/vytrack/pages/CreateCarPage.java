16c3924e5ff07ddf06f84cfdd858983e324eb899
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-10-25 11:32:56 -0400

    updated CreateCarPage class

--- src/test/java/com/vytrack/pages/CreateCarPage.java
+++ src/test/java/com/vytrack/pages/CreateCarPage.java
@@ -11,6 +11,12 @@ public class CreateCarPage extends BasePage {
     @FindBy(css = "[title='Create Car']")
     private WebElement createCarBtn;
 
+    @FindBy(name = "custom_entity_type[LicensePlate]")
+    private WebElement licencePlateInputBox;
+
+    @FindBy(name = "custom_entity_type[ModelYear]")
+    private WebElement modelYearInputBox;
+
     public void clickOnCreateCar() {
         WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 20);
         wait.until(ExpectedConditions.elementToBeClickable(createCarBtn)).click();
