95dc85e4f91408147a129e5b15589e56fea19355
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-10-25 14:17:40 -0400

    updated CreateCarPage class

--- src/test/java/com/vytrack/step_definitions/CommonStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/CommonStepDefinitions.java
@@ -1,14 +1,20 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.pages.BasePage;
 import com.vytrack.pages.LoginPage;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 
 public class CommonStepDefinitions {
-    LoginPage loginPage = new LoginPage();
+    BasePage basePage = new LoginPage();
 
     @Given("user navigates to {string} and {string}")
     public void user_navigates_to_and(String string, String string2) {
-        loginPage.navigateTo(string, string2);
+        basePage.navigateTo(string, string2);
     }
 
+    @When("user clicks on save and close button")
+    public void user_clicks_on_save_and_close_button() {
+        basePage.clickSaveAndClose();
+    }
 }
