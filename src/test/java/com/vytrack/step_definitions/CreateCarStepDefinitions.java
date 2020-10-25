0edf56196728b5d20831a2c0908df6bd2ba1e7ad
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-10-25 12:20:13 -0400

    updated CreateCarPage class

--- src/test/java/com/vytrack/step_definitions/CreateCarStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/CreateCarStepDefinitions.java
@@ -3,6 +3,7 @@ package com.vytrack.step_definitions;
 import com.vytrack.pages.CreateCarPage;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
+
 import java.util.Map;
 
 public class CreateCarStepDefinitions {
@@ -14,30 +15,38 @@ public class CreateCarStepDefinitions {
     }
 
     /**
-     *  When user adds new vehicle information
-     *       | License Plate | SDET |
-     *       | Model Year    | 2021 |
-     *          key           value
-     *      map it's data structure where every value is referenced by key
-     *
-     *      (in arraylist it's always index)
-     *      dataTable = { License Plate = SDET,
-     *                    Model Year = 2021 }
-     *
-     *      If you want to turn data table into map, it must be exactly 2 columns
-     *
-     *      If it's a 1 column, it can be just List:
-     *
-     *      Then user verifies following list:
-     *          |10|
-     *          |20|
-     *          |50|
+     * When user adds new vehicle information
+     * | License Plate | SDET |
+     * | Model Year    | 2021 |
+     * key           value
+     * <p>
+     * dataTable.get("License Plate") --> SDET
+     * dataTable.get("Model Year")    --> 2021
+     * <p>
+     * map it's data structure where every value is referenced by key
+     * <p>
+     * (in arraylist it's always index)
+     * dataTable = { License Plate = SDET,
+     * Model Year = 2021 }
+     * <p>
+     * If you want to turn data table into map, it must be exactly 2 columns
+     * <p>
+     * If it's a 1 column, it can be just List:
+     * <p>
+     * Then user verifies following list:
+     * |10|
+     * |20|
+     * |50|
      *
      * @Then("user verifies following list:")
      * public void user_verifies_following_list(List<String> dataTable){}
      */
     @When("user adds new vehicle information")
     public void user_adds_new_vehicle_information(Map<String, String> dataTable) {
+        //to get all keys and values one by one
+        dataTable.forEach((k, v) -> System.out.println("Key: " + k + ", value: " + v));
 
+        System.out.println("License plate: " + dataTable.get("License Plate"));
+        System.out.println("Model year: " + dataTable.get("Model Year"));
     }
 }
