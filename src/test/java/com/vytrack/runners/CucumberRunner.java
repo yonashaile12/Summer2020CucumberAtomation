3b2bb736e3db596cec55da0b879ec041ca4fa545
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-10-25 16:59:41 -0400

    updated CreateCarPage class

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
-        dryRun = false,
+        dryRun = true,
         tags = "@calendar_events",
         publish = true
 )
--- src/test/java/com/vytrack/step_definitions/CreateCalendarEventStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/CreateCalendarEventStepDefinitions.java
@@ -2,7 +2,9 @@ package com.vytrack.step_definitions;
 
 import com.vytrack.pages.CreateCalendarEventPage;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 import java.util.Map;
 
@@ -25,5 +27,15 @@ public class CreateCalendarEventStepDefinitions {
 
         createCalendarEventPage.enterDescription(description);
         createCalendarEventPage.enterTitle(title);
+    }
+
+    @Then("user verifies that new calendar event is displayed:")
+    public void user_verifies_that_new_calendar_event_is_displayed(Map<String, String> data) {
+        String title = data.get("Title");
+        String description = data.get("Description");
+
+        Assert.assertEquals(title, createCalendarEventPage.getDataFromGeneralInfo("Title"));
+        Assert.assertEquals(description, createCalendarEventPage.getDataFromGeneralInfo("Description"));
+
     }
 }
