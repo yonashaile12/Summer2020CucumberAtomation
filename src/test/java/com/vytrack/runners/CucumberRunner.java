7e7d23e8847cab5607edf3e755bea82da21e842f
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-11-04 21:52:58 -0500

    Scenario Outline

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
         dryRun = false,
-        tags = "@add_car_scenario_outline",
+        tags = "@with_two_columns",
         publish = true
 )
 public class CucumberRunner {
