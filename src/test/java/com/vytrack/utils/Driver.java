5b3a2379ee53ab0f78469cb24da4d0ddd8757dd1
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-11-10 20:53:07 -0500

    added maven surefire plugin

--- src/test/java/com/vytrack/utils/Driver.java
+++ src/test/java/com/vytrack/utils/Driver.java
@@ -49,6 +49,16 @@ public class Driver {
                         e.printStackTrace();
                     }
                     break;
+                case "remote-firefox":
+                    try {
+                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                        desiredCapabilities.setBrowserName("firefox");
+                        URL gridUrl = new URL("http://3.82.5.142:4444/wd/hub");
+                        driver = new RemoteWebDriver(gridUrl, desiredCapabilities);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
                 default:
                     throw new RuntimeException("No such a browser yet!");
             }
