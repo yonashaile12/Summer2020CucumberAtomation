52a7af2238ae1dd9cd0fc6f6538d018da524d9c3
git config --global user.name <yonashaile12@gmail.com>
2020-11-14 10:06:19 -0600

    driver class updated

--- src/test/java/com/vytrack/runners/CucumberRunner2.java
+++ src/test/java/com/vytrack/runners/CucumberRunner2.java
@@ -9,18 +9,17 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {
                 "rerun:target/rerun.txt",
-                "json:target/cucumber.json",
-                "timeline:target/timeline-report"
+                "json:target/cucumber2.json"
         },
         features = {
-                "src/test/resources/features/activities"
+                "src/test/resources/features/fleet"
         },
         glue = "com/vytrack/step_definitions",
         dryRun = false,
         //tags = "",
         publish = true
 )
-public class CucumberRunner {
+public class CucumberRunner2 {
     //tags = "@s_o and @with_two_columns",
     // scenario must have BOTH tags
     //java logic if(@s_o && @with_two_columns)
